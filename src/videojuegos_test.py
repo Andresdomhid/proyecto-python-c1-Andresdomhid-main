bf14604044605c2983a6e2cb1a140d318ccdff9b
Andres Dominguez <anddomhid@alum.us.es>
2022-11-12 22:13:56 +0100

    .

--- src/videojuegos_test.py
+++ src/videojuegos_test.py
@@ -1,9 +1,10 @@
 
-from videojuegos import *
+#from videojuegos import *
+from p2 import *
 
 #lee_ficheros(videojuegos)
 
 #Juegos_Wii = filtra_por_plataforma(videojuegos, 'Wii')
 #print(Juegos_Wii)
 
-print(plataformas)
+lee_datos('./data/Video_Game_Sales_as_of_Jan_2017.csv')
