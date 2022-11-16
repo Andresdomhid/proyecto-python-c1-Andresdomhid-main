47a19982012416096e43153d863e6e2fcde6e030
Andres Dominguez <anddomhid@alum.us.es>
2022-11-16 14:28:05 +0100

    .

--- src/test_videojuegos.py
+++ src/test_videojuegos.py
@@ -7,12 +7,12 @@ DATOS = lee_datos('./data/Video_Game_Sales_as_of_Jan_2017.csv')
 #calcula_media_valoracion_por_juego()
 
 def test_media_valoracion_por_juegos():
-    print(lee_datos(DATOS))
+    print(DATOS)
 
 def main():
     test_media_valoracion_por_juegos()
 
-    
+
 if __name__ == '__main__':
     main()
 
