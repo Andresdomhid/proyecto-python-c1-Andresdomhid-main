9734dede2be6a7e7a4a806c2fbeadbf8f4d43e65
Andres Dominguez <anddomhid@alum.us.es>
2022-11-24 19:29:05 +0100

    .

--- src/test_videojuegos.py
+++ src/test_videojuegos.py
@@ -4,15 +4,22 @@ DATOS = lee_datos('./data/Video_Game_Sales_as_of_Jan_2017.csv')
 def test_lee_datos():
     print(DATOS)
 
+#TEST 1#
 def test_filtra_clasificcion_por_edades():
     print(filtra_clasificcion_por_edades(DATOS))
 
+#TEST2#
 def test_media_valoracion_por_juegos():
     print(calcula_media_valoracion_por_juego(DATOS, 'NBA 2K17'))
 
-def test_agrupa_videojuegos_por_anyo():
-    print(agrupar_videojuegos_por_anyo(DATOS))
+#TEST3#
+def test_valor_maximo_ventas_juegos_por_anyo():
+    print(valor_maximo_ventas_juegos_por_anyo(DATOS, 1998))
 
+
+
+
+##############################################################################################################################
 def test_valor_maximo_ventas_juegos_por_Plataforma():
     print(valor_maximo_ventas_juegos_por_Plataforma(DATOS, 'Wii'))
     print()
@@ -20,7 +27,8 @@ def test_valor_maximo_ventas_juegos_por_Plataforma():
 def test_agrupar_videojuegos_por_anyo():
     print(agrupar_videojuegos_por_anyo(DATOS, 'Dino Crisis'))
 
-
+def test_agrupa_videojuegos_por_anyo():
+    print(agrupar_videojuegos_por_anyo(DATOS))
 
 
 def test_maximo_ventas_por_plataforma():
@@ -29,9 +37,18 @@ def test_maximo_ventas_por_plataforma():
 
 def test_maximo_ventas_por_anyo_y_plataforma():
     print(maximo_ventas_por_anyo_y_plataforma(DATOS))
+###############################################################################################################################
+
+
+
+
+def test_agrupar_juegos_por_genero():
+    print(agrupar_juegos_por_genero(DATOS, 'Fighting'))
+
+
 
 def main():
-   test_valor_maximo_ventas_juegos_por_Plataforma()
+  test_agrupar_juegos_por_genero()
 
 
 if __name__ == '__main__':
